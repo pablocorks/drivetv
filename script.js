2a716f9a462ee254d9d6722fc55cbb677a168282
pablocorks <pablomrx@gmail.com>
2025-06-01 10:53:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const apiUrl = "https://script.google.com/macros/s/SEU_ID_DO_SCRIPT/exec";
+const videoListEl = document.getElementById("videoList");
+const playerEl = document.getElementById("player");
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    data.forEach((video, index) => {
+      const li = document.createElement("li");
+      li.textContent = video.name;
+      li.onclick = () => {
+        playerEl.src = video.url;
+        playerEl.play();
+      };
+      videoListEl.appendChild(li);
+
+      if (index === 0) {
+        playerEl.src = video.url;
+      }
+    });
+  })
+  .catch(err => {
+    console.error("Erro ao buscar vídeos:", err);
+  });
+
+function toggleFullscreen() {
+  document.querySelector('.video-list').style.display = 'none';
+  document.querySelector('.video-player').style.width = '100%';
+}
